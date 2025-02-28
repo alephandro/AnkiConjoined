2949f7eb40a0093f47e7291056f0d28ef0481279
alephandro <alex29561@gmail.com>
2025-02-28 13:06:48 +0100

    Update client.py and server.py connection and data exchange

--- Client/client.py
+++ Client/client.py
@@ -1,16 +1,23 @@
 import socket
+import json
+from testAnkiConnected import get_cards_from_deck
 
 class Client:
-
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-
+    def __init__(self, host="localhost", port=9999):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((self.host, self.port))
+        self.sock.connect((host, port))
+        print("Connected to server.")
 
-        print("Connected", self)
+    def send_cards(self):
+        print("Fetching cards...")
+        cards = get_cards_from_deck("TestDeck", 0)
+
+        json_data = json.dumps(cards)
+        self.sock.sendall(json_data.encode("utf-8"))
+
+        print("Cards sent successfully.")
+        self.sock.close()
 
 # --- main ---
-
-Client("localhost", 9999)
+client = Client()
+client.send_cards()
--- Server/server.py
+++ Server/server.py
@@ -1,31 +1,44 @@
 import socket
 import threading
+import json
 
 
 class Server:
-    def __init__(self):
+    def __init__(self, host="localhost", port=9999):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind(("localhost", 9999))
-        self.sock.listen(1)
+        self.sock.bind((host, port))
+        self.sock.listen()
+
+        print("Server listening on", (host, port))
 
         while True:
             try:
-                print("Waiting for connection...")
                 conn, addr = self.sock.accept()
                 print("Connected to", addr)
-                threading.Thread(self.handle_client()).start()
-            except Exception as e:
-                print(e)
+                threading.Thread(target=self.handle_client, args=(conn,)).start()
+            except KeyboardInterrupt:
+                break
             finally:
-                if socket:
-                    socket.close()
+                print("Closing server")
+                self.sock.close()
 
+    def handle_client(self, conn):
+        try:
+            data = conn.recv(4096)
+            if not data:
+                return
 
+            decoded_data = data.decode("utf-8")
+            cards = json.loads(decoded_data)
 
-    def handle_client(connection):
-        print("Connected", connection)
+            print("I received", len(cards), "cards")
+            print("Received cards:", cards)
+
+        except Exception as e:
+            print("Error:", e)
+        finally:
+            conn.close()
+
 
 # --- main ---
-
 Server()
-
