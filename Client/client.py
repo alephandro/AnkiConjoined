c6478d3fa3697a900914b4132818b936a5505c38
alephandro <alex29561@gmail.com>
2025-03-03 15:17:05 +0100

    Create receive_cards() function

--- Client/client.py
+++ Client/client.py
@@ -1,7 +1,10 @@
 import socket
 import json
 
+from DataManagement.cards_management import collect_cards
 from testAnkiConnected import get_cards_from_deck
+from testAnkiConnected import sync_card
+
 
 class Client:
     HEADER = 64
@@ -14,23 +17,11 @@ class Client:
     def send_cards(self, deck_name):
         print("Fetching cards...")
         cards = get_cards_from_deck(deck_name, 0)
-        '''
-        Option to send cards --> code 0
-        Send deck name
-        Send cards
-        '''
         try:
             self.sock.sendall(str(0).encode("utf-8"))
-
-            deck_name_encoded = deck_name.encode("utf-8")
-            deck_name_length = str(len(deck_name_encoded)).encode("utf-8")
-            deck_name_length += b' ' * (self.HEADER - len(deck_name_length))
-            self.sock.sendall(deck_name_length)
-            self.sock.sendall(deck_name_encoded)
-
+            self.send_size_and_package(deck_name)
             json_data = json.dumps(cards)
             self.sock.sendall(json_data.encode("utf-8"))
-
             self.sock.shutdown(socket.SHUT_WR)
 
             response = self.sock.recv(1).decode("utf-8")
@@ -44,7 +35,37 @@ class Client:
         finally:
             self.sock.close()
 
+    def receive_cards(self, deck_name, timestamp):
+        try:
+            self.sock.sendall(str(1).encode("utf-8"))
+            self.send_size_and_package(deck_name)
+            self.send_size_and_package(timestamp)
+            self.sock.shutdown(socket.SHUT_WR)
+
+            cards = collect_cards(self.sock)
+            for key, value in cards.items():
+                sync_card(value)
+
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            self.sock.close()
+
+
+    def send_size_and_package(self, info):
+        if not isinstance(info, str):
+            info = str(info)
+
+        info_encoded = info.encode("utf-8")
+        info_length = str(len(info_encoded)).encode("utf-8")
+        info_length += b' ' * (self.HEADER - len(info_length))
+        self.sock.sendall(info_length)
+        self.sock.sendall(info_encoded)
+
 # --- main ---
 if __name__ == "__main__":
     client = Client()
-    client.send_cards('Kaishi 1.5k')
+    client.send_cards("TestDeck")
+    # client.receive_cards("TestDeck", str(1740659710367))
+
+
